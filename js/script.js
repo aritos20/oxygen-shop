509b375c5f9dcaa7b392911cd1995524023db2c2
aritos20 <agianicopulos@gmail.com>
2022-12-20 14:41:26 +0100

    scroll percentage bar functionality added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Burger menu
+
 const burgerMenu = document.querySelector('#burger-menu');
 const dropDown = document.querySelector('.header__nav-options');
 const imgBurgerMenu = document.querySelector('.header__burger-menu');
@@ -8,3 +10,24 @@ burgerMenu.addEventListener('click', () => {
     imgBurgerMenu.classList.toggle('active');
     imgXMenu.classList.toggle('active');
 });
+
+// Scroll percentage bar 
+
+const progressBar = document.querySelector('.progress-bar');
+const body = document.querySelector('body');
+
+const scrollProgressBar = () => {
+    let scrollDistance = -(body.getBoundingClientRect().top);
+    let progressPercentage = (scrollDistance / (body.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100;
+    let val = Math.floor(progressPercentage);
+    console.log(val);
+
+    progressBar.style.width = val + '%';
+
+    if (val < 0) {
+        progressBar.style.width = '0%';
+    }
+};
+
+window.addEventListener('scroll', scrollProgressBar);
+
