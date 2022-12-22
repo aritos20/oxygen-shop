2843e3b8dcce29ac27cb7142d4050fd253f6a856
aritos20 <agianicopulos@gmail.com>
2022-12-22 11:59:15 +0100

    modal functionality in progress and added some new things to the form validation

--- js/script.js
+++ js/script.js
@@ -41,13 +41,16 @@ arrow.addEventListener('click', () => {
             behavior: 'smooth',
         })
     }, 200);
-})
+});
 
 // Form validation
 
 const formName = document.querySelector('#name');
 const formEmail = document.querySelector('#email');
+const sendButton = document.querySelector('#send-form');
+const checkbox = document.querySelector('#checkbox');
 const emailRegexValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 
 formName.addEventListener('input', () => {
     if (formName.value.length >= 2 && formName.value.length <= 100) {
@@ -61,7 +64,6 @@ formName.addEventListener('input', () => {
 });
 
 formEmail.addEventListener('input', () => {
-    console.log(typeof formEmail.value)
     if (emailRegexValidation.test(formEmail.value)) {
         formEmail.style.borderBlockColor = "green";
     } else {
@@ -70,3 +72,36 @@ formEmail.addEventListener('input', () => {
         formEmail.style.borderBlockColor = "#A5A5A5";
     }
 });
+
+sendButton.addEventListener('click', () => {
+    if (!emailRegexValidation.test(formEmail.value) || !(formName.value.length >= 2 && formName.value.length <= 100)) {
+        alert('Please enter a valid name or email');
+    }
+    if (!checkbox.checked) {
+    alert('Please accept the terms and conditions by marking the checkbox');
+    }
+});
+
+// fetch api use for the form data
+
+const jsonPlaceholder = async () => {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: formName.value,
+                email: formEmail.value
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',  
+            },
+        })
+        if (response.ok) {
+            const jsonResponse = await response.json();
+        }
+    } catch (error) {
+        alert(error);
+    }
+}
+
+sendButton.addEventListener('click', jsonPlaceholder);
